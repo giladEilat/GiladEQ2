36ac8c7f01f7e883b0efcddac8e17219dba7a66c
giladEilat <gilad.eilat@gmail.com>
2024-11-09 15:36:57 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,28 @@ public class Main {
     // פעולה לבדיקת מספר קפרקר
     public static boolean isKaprekar(int num) {
         // TODO: השלם את הלוגיקה לבדיקת מספר קפרקר
-        return false; // החזרה זמנית
+        if (numLen(num)%2!=0)
+            return false;
+        int[]arr = numSeparate(num);
+        if (arr[0]+arr[1]==num)
+            return true;
+        return false;
+    }
+    public static int numLen(int num){
+        int len = 0;
+        int sq = (int)Math.pow(num,2);
+        while(sq!=0){
+            len++;
+            sq = (int)(sq/10);
+        }
+        return len;
+    }
+    public static int[] numSeparate(int num){
+        int pwoer = numLen(num);
+        int sq = (int)Math.pow(num,2), factor =(int) Math.pow(10.0,(double)pwoer/2);
+        int[]arr = new int[2];
+        arr[0] = sq/factor;
+        arr[1] = sq%factor;
+        return arr;
     }
 }
